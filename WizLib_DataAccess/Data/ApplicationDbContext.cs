15b3055f95369d81d3051add852589e2dcef4664
TomEnglish <tomenglish23@gmail.com>
2021-09-25 09:59:42 -0400

    First commit

---
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WizLib_Model.Models;
+
+namespace WizLib_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        //public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ WizLib_DataAccess/Migrations/20210924213820_changeNameToGenreNameinGenreTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class changeNameToGenreNameinGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Genres",
+                newName: "GenreName");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Genres",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Genres");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreName",
+                table: "Genres",
+                newName: "Name");
+        }
+    }
+}
